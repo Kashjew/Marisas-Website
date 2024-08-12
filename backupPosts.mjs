df6e213a8c0f54882a23228c9aea7ec9b6feff00
Kashjew <yegorkushnir1@gmail.com>
2024-08-11 23:51:28 -0400

    Updated backupPosts.mjs to use environment variables for AWS credentials

--- backupPosts.mjs
+++ backupPosts.mjs
@@ -1,14 +1,13 @@
 import { S3Client, PutObjectCommand } from "@aws-sdk/client-s3";
 import https from 'https';
-import { fromIni } from "@aws-sdk/credential-providers"; // Assumes you're using environment variables
 
 // AWS S3 configuration
 const s3 = new S3Client({
     region: process.env.AWS_REGION,
-    credentials: fromIni({
+    credentials: {
         accessKeyId: process.env.AWS_ACCESS_KEY_ID,
         secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
-    }),
+    }
 });
 
 const bucketName = process.env.S3_BUCKET_NAME;
