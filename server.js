4d8de46c2abe07e78de8c60c1e6b133e785d35c6
Yegor Kushnir <yegork@DESKTOP-B895SCD>
2024-10-30 00:18:21 -0400

    deploy dammit

--- server.js
+++ server.js
@@ -3,7 +3,7 @@
 const app = require('./app');  // Make sure this imports app correctly, including all middleware and routers
 const connectDB = require('./config/database'); // Import the connectDB function to connect to MongoDB
 
-// Async function to initialize services and start the server
+// Async function to initialize services without listening on a port
 (async () => {
   try {
     // Connect to MongoDB
@@ -11,14 +11,10 @@ const connectDB = require('./config/database'); // Import the connectDB function
     await connectDB(); // Ensure connectDB is an async function that returns a promise
     console.log("MongoDB connection successful");
 
-    // Start the server
-    const port = process.env.PORT || 3000;
-    app.listen(port, () => {
-      console.log(`Server is running on port ${port}`);
-    });
-
   } catch (error) {
     console.error("Error during server setup:", error);
     process.exit(1); // Exit the process with failure
   }
 })();
+
+module.exports = app;
